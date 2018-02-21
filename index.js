f7b8fa61bbdd769efd7e89e8d2c376f88c4e5f2f
Tiffany Schureman <Tiffany@Tiffanys-MacBook-Air.local>
2018-02-21 13:44:20 -0600

    step 1-4 done

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+
+const strategy = require(`${__dirname}/strategy`);
 
 const app = express();
+
 app.use( session({
   secret: '@nyth!ng y0u w@nT',
   resave: false,
   saveUninitialized: false
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser((user,done) => {
+  done(null,user);
+});
+
+passport.deserializeUser((user,done) =>{
+  done(null,user);
+})
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
